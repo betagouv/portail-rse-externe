2c9810fb56d4a742269d80e968ab23d2e37b5860
Frédéric Vergez <fred@ikarius.com>
2025-03-14 11:53:24 +0100

    fix: suppression des routes inutilisées
    Le nettoyage des fichiers est automatique en fin de traitement

--- esg-api/src/flask_app/app.py
+++ esg-api/src/flask_app/app.py
@@ -12,20 +12,15 @@ MEF/360Client
 
 import logging
 import os
-from functools import wraps
-from pathlib import PosixPath
 
-import fitz
 import requests
-from flask import abort, json, make_response, request, send_file
+from flask import request, jsonify
 from flask_jwt_extended import JWTManager, jwt_required
 
 import flask_app.tasks as tasks
 
 # voir :__init__.py
 from flask_app import (
-    CURRENT_FULL_PATH,
-    TEXT_FILE_NAME_CSV,
     WS_PATH,
     init_flask_app,
 )
@@ -36,164 +31,22 @@ jwt = JWTManager(app)
 logger = logging.getLogger(__name__)
 
 
-# jsonify API responses
-def json_response(msg_dict, status=200, indent=4, sort_keys=True):
-    response = make_response(
-        json.dumps(msg_dict, ensure_ascii=False, indent=indent, sort_keys=sort_keys)
-    )
-    response.headers["Content-Type"] = "application/json; charset=utf-8"
-    response.headers["mimetype"] = "application/json"
-    response.status_code = status
-    return response
+def _fetch_s3_document(url: str, document_id: str) -> str:
+    response = requests.get(url)
+    pdf_path = f"{WS_PATH}/document_{document_id}"
+    file_path = f"{pdf_path}/fichier.pdf"
+
+    os.makedirs(pdf_path, exist_ok=True)
+
+    with open(file_path, "wb") as f:
+        f.write(response.content)
+
+    return pdf_path
 
 
-# Delete all pdfs from workspace
-def deltree(target):
-    for d in os.listdir(target):
-        try:
-            deltree(target + "/" + d)
-        except OSError:
-            os.remove(target + "/" + d)
-    os.rmdir(target)
-
-
-# Check if a file is a pdf
-def check_pdf_in_path(pdf_path):
-    PDFs_DIR = PosixPath(pdf_path)
-    pdfs = list(PDFs_DIR.glob("*.pdf"))
-
-    if len(pdfs) == 1:
-        # Check .pdf File is not a PDF
-        try:
-            # Attempt to open the file
-            doc = fitz.open(pdfs[0])
-
-            # Check the PDF page count
-            if doc.page_count > 0:
-                return 0
-            else:
-                return -1
-        except Exception:
-            return -1
-    else:
-        # Too many .pdf files found
-        return -3
-
-
-# Check if pdf token is given
-def pdf_token_required(f):
-    @wraps(f)
-    def decorator(*args, **kwargs):
-        if request.method == "POST":
-            pdf_key = request.form.get("pdfkey")
-        else:
-            pdf_key = request.args.get("pdfkey")
-
-        # Check if a pdf_key is given
-        if pdf_key is None:
-            status_dict = {"code": -1, "msg": "PDF key is empty"}
-            resp_dict = {"status": status_dict}
-            return json_response(resp_dict)
-        else:
-            # Path to pdf file
-            pdf_path = WS_PATH + "/" + "pdf_" + pdf_key
-
-            # Check if pdf_key path exists
-            if os.path.exists(pdf_path):
-                # Check if the uploaded file is a pdf
-                ret = check_pdf_in_path(pdf_path)
-
-                if ret == -1:
-                    status_dict = {"code": -1, "msg": ".pdf file is not a PDF file"}
-                    resp_dict = {"status": status_dict}
-                    return json_response(resp_dict)
-
-                elif ret == -2:
-                    status_dict = {"code": -1, "msg": "No .pdf file found"}
-                    resp_dict = {"status": status_dict}
-                    return json_response(resp_dict)
-
-                elif ret == -3:
-                    status_dict = {"code": -1, "msg": "Too many .pdf files found"}
-                    resp_dict = {"status": status_dict}
-                    return json_response(resp_dict)
-
-                else:
-                    return f(pdf_key, pdf_path, *args, **kwargs)
-
-            else:
-                status_dict = {"code": -1, "msg": "PDF key not found"}
-                resp_dict = {"status": status_dict}
-                return json_response(resp_dict)
-
-    return decorator
-
-
-@app.errorhandler(400)
-def page_not_found(_):
-    return "bad request!", 400
-
-
-@app.route("/")
-def index():
-    status_dict = {"code": 0, "msg": "Welcome to the ESRS API"}
-    resp_dict = {"status": status_dict}
-    return json_response(resp_dict)
-
-
-@app.route("/ping", methods=["GET", "POST"])
+@app.route("/ping", methods=["GET"])
 def ping():
-    status_dict = {"code": 0, "msg": "API is alive"}
-    resp_dict = {"status": status_dict}
-    return json_response(resp_dict)
-
-
-# TODO: pas convaincu de l'intérêt de cette route
-# le nettoyage pourrait être effectué à chaque succés
-@app.route("/clean", methods=["GET", "POST"])
-@pdf_token_required
-@jwt_required()
-def clean(pdf_key, pdf_path):
-    status_dict = {"code": 0, "msg": "Cleaning done"}
-    resp_dict = {"status": status_dict}
-
-    # Delete pdf_key directory
-    deltree(pdf_path)
-
-    return json_response(resp_dict)
-
-
-# TODO: pas convaincu de l'intérêt de cette route
-@app.route("/gettxtfile", methods=["GET", "POST"])
-@pdf_token_required
-@jwt_required()
-def gettxtfile(pdf_key, pdf_path):
-    # Get texts csv file from pdf path
-    text_file_path = (
-        str(CURRENT_FULL_PATH) + "/" + str(pdf_path) + "/" + TEXT_FILE_NAME_CSV
-    )
-
-    try:
-        return send_file(text_file_path, as_attachment=True)
-
-    except FileNotFoundError:
-        abort(404)
-
-
-# Note: que se passe t'il si une prédiction est en cours ?
-@app.route("/cleanall", methods=["GET", "POST"])
-@jwt_required()
-def clean_all():
-    # Delete bert directory and temporary files
-    if os.path.exists(WS_PATH):
-        deltree(WS_PATH)
-
-    # Create an empty workspace
-    os.makedirs(WS_PATH)
-
-    status_dict = {"code": 0, "msg": "Cleaning done"}
-    resp_dict = {"status": status_dict}
-    return json_response(resp_dict)
+    return jsonify({"status": "alive", "msg": "API is alive and well"})
 
 
 @app.route("/run-task", methods=["POST"])
@@ -203,18 +56,12 @@ def run_task():
     try:
         document_id = request.form["document_id"]
         s3_url = request.form["url"]
-        response = requests.get(s3_url)
-        pdf_path = f"{WS_PATH}/document_{document_id}"
-        os.makedirs(pdf_path, exist_ok=True)
-        file_path = f"{pdf_path}/fichier.pdf"
-
-        with open(file_path, "wb") as f:
-            f.write(response.content)
+        pdf_path = _fetch_s3_document(s3_url, document_id)
     except Exception as ex:
         logger.exception(ex)
-        return json_response({"status": "error", "msg": str(ex)})
+        return jsonify({"status": "error", "msg": str(ex)})
     else:
         # à ce point, les erreurs sont gérées par la tâche Celery
         tasks.analyser.delay(document_id, pdf_path)
 
-        return json_response({"status": "en attente"})
+        return jsonify({"status": "en attente"})
