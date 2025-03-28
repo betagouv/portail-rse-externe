26beae44253f2e736614b1305b712411007efacd
Frédéric Vergez <fred@ikarius.com>
2025-03-28 18:21:48 +0100

    feat: ajout d'un script de déploiement
    Reprends simplement une à une les étapes manuelles de reconstruction de
    l'image podman à partir des dernières sources de main

---
+++ esg-api/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+echo "🚀 Début du script de déploiement"
+
+echo "🔄 Mise à jour du code depuis Github"
+git checkout main
+git pull origin main
+
+echo "🛑 Arrêt de l'ancien conteneur"
+podman-compose down
+
+echo "🧱 Reconstruction de l'image et lancement du nouveau conteneur"
+podman-compose up -d --remove-orphans 
+
+echo "🧹 Nettoyage des anciennes images"
+podman image prune -f
+
+echo "🎉 Fin du script de déploiement"
+
+exit 0
